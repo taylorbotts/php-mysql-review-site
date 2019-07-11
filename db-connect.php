5dddb6e7239fd655a9a59bc7097230f305fbdded
Taylor <42750609+taylorbotts@users.noreply.github.com>
2019-07-11 13:29:13 -0400

    Update db-connect.php

--- db-connect.php
+++ db-connect.php
@@ -2,7 +2,7 @@
     if (($_SERVER['SERVER_ADDR'] == '::1') || ($_SERVER['SERVER_ADDR'] == '127.0.0.1')){
         $mysqli = new mysqli('localhost', 'root', '', 'dig3134'); }
     else {
-        $mysqli = new mysqli('students.cah.ucf.edu','ta741447','dY%@A&%evku9ktT53','ta741447');
+        // Connect to UCF database
   }
     if($mysqli->error) {
         print "MySQL Error: ".$mysqli->error;
